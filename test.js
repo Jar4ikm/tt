fb7ba2b6e375acbb6c37b7368719123d81863f6a
Jar4ik <119585243+Jar4ikm@users.noreply.github.com>
2024-10-28 16:26:48 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
 
+const request = require('supertest');
+const app = require('./app'); // путь к app.js должен быть './app'
+
+describe('GET /', function () {
+  it('should respond with Hello World and status 200', function (done) {
+    request(app)
+      .get('/')
+      .expect(200)
+      .expect('Hello World', done);
+  });
+});
+
